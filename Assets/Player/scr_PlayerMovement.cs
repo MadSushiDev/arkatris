71dd0dc45416ec41e55fd35373f8a66e7feb249c
Rubén Izquierdo <info@madsushigames.com>
2023-01-04 14:44:12 +0100

    Se ha creado la pala del jugador con el movimiento base.

---
+++ Assets/Player/scr_PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    private float inputValue;
+    public float speed = 25;
+    private Vector2 direction;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputValue = Input.GetAxisRaw("Horizontal");
+
+        if (inputValue == 1) {
+            direction = Vector2.right;
+        } else if (inputValue == -1) {
+            direction = Vector2.left;
+        } else {
+            direction = Vector2.zero;
+        }
+
+        rb.AddForce(direction*speed*Time.deltaTime*100);
+    }
+}
