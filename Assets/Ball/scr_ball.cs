2dec79c09bd6cc50aab01138761c7b6f0afd40d6
Rubén Izquierdo <info@madsushigames.com>
2023-01-04 17:27:17 +0100

    Se han hecho mejoras en el gameplay de la bola.
    Ahora la bola rebota de una forma más predecible.

--- Assets/Ball/scr_ball.cs
+++ Assets/Ball/scr_ball.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class scr_ball : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public float ballSpeed = 10;
+    public float ballSpeed = 25;
+
+
+    private Vector2 velocity;
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +24,20 @@ public class scr_ball : MonoBehaviour
     void InitiateBall() {
         rb.velocity = new Vector2 (0,0);
         transform.position = new Vector2(0, 0);
-        rb.AddForce(Vector2.down * ballSpeed * Time.deltaTime * 100);
+        velocity.x = Random.Range(-1, 1);
+        velocity.y = 1;
+        rb.AddForce(velocity * ballSpeed);
     }
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "DeadEnd") {
             Debug.Log("Dead");
             InitiateBall();
+        }
+        if (collision.gameObject.name == "obj_Player") {
+            float hitFactor(Vector2 ballPos, Vector2 racketPos, float racketWidth) { return (ballPos.x - racketPos.x) / racketWidth; }
+            float x = hitFactor(transform.position, collision.transform.position, collision.collider.bounds.size.x);
+            Vector2 dir = new Vector2(x, 1).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir * ballSpeed/2;
         }
 
     }
